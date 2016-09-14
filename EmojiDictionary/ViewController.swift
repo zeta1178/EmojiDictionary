1f2fe7a27f015f75098bd51f5f7170be8103b13c
Michael Cruz <michaelcruz@aim.com>
2016-09-14 08:53:15 -0400

    working with embedded data

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -97,43 +97,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
-    
-    func makeDataArray() -> [DataC] {
-        let data1 = DataC()
-        data1.firstName = "Michael"
-        data1.lastName = "Cruz"
-        data1.url   =   "test1"
         
-        let data2 = DataC()
-        data2.firstName = "Vanessa"
-        data2.lastName = "Setton"
-        data2.url   =   "test2"
-        
-        let data3 = DataC()
-        data3.firstName = "Vanessa"
-        data3.lastName = "Setton"
-        data3.url   =   "test2"
-        
-        let data4 = DataC()
-        data4.firstName = "Vanessa"
-        data4.lastName = "Setton"
-        data4.url   =   "test2"
-        
-        let data5 = DataC()
-        data5.firstName = "Vanessa"
-        data5.lastName = "Setton"
-        data5.url   =   "test2"
-        
-        let data6 = DataC()
-        data6.firstName = "Vanessa"
-        data6.lastName = "Setton"
-        data6.url   =   "test2"
-        
-        return [data1,data2,data3,data4,data5,data6]
-        
-    }
-    
-    
     func makeEmojiArray() -> [EmojiC] {
         
         let emoji1 = EmojiC()
---
+++ EmojiDictionary/makeData.swift
@@ -0,0 +1,42 @@
+//
+//  makeData.swift
+//  EmojiDictionary
+//
+//  Created by Michael Cruz on 9/13/16.
+//  Copyright © 2016 MIchael Cruz. All rights reserved.
+//
+
+func makeDataArray() -> [DataC] {
+    let data1 = DataC()
+    data1.firstName = "Michael"
+    data1.lastName = "Cruz"
+    data1.url   =   "test1"
+    
+    let data2 = DataC()
+    data2.firstName = "Vanessa"
+    data2.lastName = "Setton"
+    data2.url   =   "test2"
+    
+    let data3 = DataC()
+    data3.firstName = "Vanessa"
+    data3.lastName = "Setton"
+    data3.url   =   "test2"
+    
+    let data4 = DataC()
+    data4.firstName = "Vanessa"
+    data4.lastName = "Setton"
+    data4.url   =   "test2"
+    
+    let data5 = DataC()
+    data5.firstName = "Vanessa"
+    data5.lastName = "Setton"
+    data5.url   =   "test2"
+    
+    let data6 = DataC()
+    data6.firstName = "Vanessa"
+    data6.lastName = "Setton"
+    data6.url   =   "test2"
+    
+    return [data1,data2,data3,data4,data5,data6]
+    
+}
